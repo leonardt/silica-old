e5f033e2836c963c87a700659be38bd66a3b49cf
Lenny Truong <leonard@truong.io>
2017-01-23 09:21:34 -0800

    Rename writeup directory, add downsample example

---
+++ examples/downsampler.py
@@ -0,0 +1,15 @@
+from coroutine import coroutine
+
+@coroutine
+def downsample(size_x, size_y):
+    _output = None
+    valid   = False
+    for y in range(0, size_y):
+        for x in range(0, size_x):
+            _input  = yield _output, valid
+            _output = _input
+            valid   = x % 2
+
+gen = downsample(100, 100)
+for i in range(0, 5):
+    print(gen.send(i))
