1df65f6a274864cd27b53195503ceaae15e136ed
Lenny Truong <leonard@truong.io>
2017-02-06 10:29:53 -0800

    Introduce concept of branch node

--- silica/cfg.py
+++ silica/cfg.py
@@ -4,9 +4,22 @@ import astor
 class BasicBlock:
     def __init__(self):
         self.statements = []
+        self.outgoing_edges = set()
+        self.incoming_edges = set()
 
     def add(self, stmt):
         self.statements.append(stmt)
+
+    def add_outgoing_edge(self, sink, label):
+        self.outgoing_edges.add((sink, label))
+
+    def add_incoming_edge(self, source, label):
+        self.incoming_edges.add((source, label))
+
+class Branch(BasicBlock):
+    def __init__(self, cond):
+        super().__init__()
+        self.cond = cond
 
 
 class ControlFlowGraph(ast.NodeVisitor):
@@ -14,7 +27,6 @@ class ControlFlowGraph(ast.NodeVisitor):
     def __init__(self, ast):
         super()
         self.blocks = []
-        self.edges = []
         self.curr_block = None
 
         self.visit(ast)
@@ -24,15 +36,32 @@ class ControlFlowGraph(ast.NodeVisitor):
         self.blocks.append(block)
         return block
 
+    def new_branch(self, test):
+        block = Branch(test)
+        self.blocks.append(block)
+        return block
+
     def add_edge(self, source, sink, label=""):
-        self.edges.append((source, sink, label))
+        source.add_outgoing_edge(sink, label)
+        sink.add_incoming_edge(source, label)
+        # self.edges.append((source, sink, label))
+
+    def outgoing_edges(block):
+        edges = []
+        for source, _, _ in self.edges:
+            if source == block:
+                edges.append(source)
+        return edges
 
     def process_stmt(self, stmt):
+        # TODO: Should be able to refactor this to reuse logic for "branching"
+        # nodes
         if isinstance(stmt, (ast.While, ast.For)):
             old_block = self.curr_block
-            self.curr_block = self.get_new_block()
+            self.curr_block = self.new_branch(stmt.test)
             self.add_edge(old_block, self.curr_block)
-            self.curr_block.add(stmt)
+            # self.curr_block.add(stmt)
+            # self.curr_block.add(ast.If(stmt.test, [], []))
             old_block = self.curr_block
             self.curr_block = self.get_new_block()
             self.add_edge(old_block, self.curr_block, label="T")
@@ -43,9 +72,9 @@ class ControlFlowGraph(ast.NodeVisitor):
             self.add_edge(old_block, self.curr_block, label="F")
         elif isinstance(stmt, (ast.If,)):
             old_block = self.curr_block
-            self.curr_block = self.get_new_block()
+            self.curr_block = self.new_branch(stmt.test)
             self.add_edge(old_block, self.curr_block)
-            self.curr_block.add(stmt)
+            # self.curr_block.add(stmt)
             old_block = self.curr_block
             self.curr_block = self.get_new_block()
             self.add_edge(old_block, self.curr_block, label="T")
@@ -67,24 +96,32 @@ class ControlFlowGraph(ast.NodeVisitor):
         else:
             self.curr_block.add(stmt)
 
+    def remove_block(self, block):
+        for source, source_label in block.incoming_edges:
+            source.outgoing_edges.remove((block, source_label))
+        for sink, sink_label in block.outgoing_edges:
+            sink.incoming_edges.remove((block, sink_label))
+        for source, source_label in block.incoming_edges:
+            for sink, sink_label in block.outgoing_edges:
+                self.add_edge(source, sink, source_label)
+
     def consolidate_empty_blocks(self):
         new_blocks = []
         for block in self.blocks:
-            if len(block.statements) > 0:
+            if len(block.statements) > 0 or isinstance(block, Branch):
                 new_blocks.append(block)
-                continue
-            sources = []
-            sinks = []
-            for source, sink, label in self.edges:
-                if sink == block:
-                    sources.append((source, label))
-            for source, sink, label in self.edges:
-                if source == block:
-                    sinks.append(sink)
-            for source, label in sources:
-                for sink in sinks:
-                    self.add_edge(source, sink, label)
-            self.edges = [x for x in self.edges if block not in x]
+            else:
+                self.remove_block(block)
+        self.blocks = new_blocks
+
+    def remove_if_trues(self):
+        new_blocks = []
+        for block in self.blocks:
+            if isinstance(block, Branch) and (isinstance(block.cond, ast.NameConstant) \
+                    and block.cond.value == True):
+                self.remove_block(block)
+            else:
+                new_blocks.append(block)
         self.blocks = new_blocks
 
     def visit_FunctionDef(self, node):
@@ -92,25 +129,21 @@ class ControlFlowGraph(ast.NodeVisitor):
         for stmt in node.body:
             self.process_stmt(stmt)
         self.consolidate_empty_blocks()
+        self.remove_if_trues()
 
     def render(self):
         from graphviz import Digraph
         dot = Digraph(name="top")
         for block in self.blocks:
-            label = ""
-            for statement in block.statements:
-                if isinstance(statement, (ast.While,)):
-                    label += "while {}:".format(astor.to_source(statement.test).rstrip())
-                elif isinstance(statement, (ast.For,)):
-                    label += "for {} in {}:".format(astor.to_source(statement.target).rstrip(), astor.to_source(statement.iter).rstrip())
-                elif isinstance(statement, (ast.If,)):
-                    label += "if {}:".format(astor.to_source(statement.test).rstrip())
-                else:
-                    label += astor.to_source(statement)
-                label += "\n"
-            dot.node(str(id(block)), label.rstrip()) 
-        for source, sink, label in self.edges:
-            dot.edge(str(id(source)), str(id(sink)), label)
+            if isinstance(block, Branch):
+                label = "if " + astor.to_source(block.cond)
+                dot.node(str(id(block)), label.rstrip(), {"shape":"box"})
+            else:
+                label = "\n".join(astor.to_source(stmt) for stmt in block.statements)
+                dot.node(str(id(block)), label.rstrip()) 
+        # for source, sink, label in self.edges:
+            for sink, label in block.outgoing_edges:
+                dot.edge(str(id(block)), str(id(sink)), label)
 
 
         dot.render("cfg.gv", view=True)
--- silica/fsm.py
+++ silica/fsm.py
@@ -1,3 +1,4 @@
+import copy
 import ast
 import astor
 import inspect
@@ -7,12 +8,26 @@ from silica.cfg import ControlFlowGraph
 from silica.ast_utils import *
 from silica.transformations import desugar_for_loops
 
+def contains_yield(statements):
+    return any(map(
+        lambda x: isinstance(x, ast.Call) and
+                  isinstance(x.func, ast.Name) and
+                  x.func.id == "yield"))
+
+def find_paths_between_yields(cfg):
+    for block in cfg.blocks:
+        if contains_yield(block.statements):
+            seen = set()
+
+
+
 class FSM:
     def __init__(self, f):
         tree = get_ast(f)
         tree = desugar_for_loops(tree)
         cfg = ControlFlowGraph(tree)
         cfg.render()
+        # paths = find_paths_between_yields(cfg)
 
 def fsm(mode_or_fn):
     if isinstance(mode_or_fn, str):
