6f6580de5106b5262aec93d04138d9c5e7ccae10
Lenny Truong <leonard@truong.io>
2017-02-06 16:19:33 -0800

    Find all paths between yields

--- silica/fsm.py
+++ silica/fsm.py
@@ -4,21 +4,27 @@ import astor
 import inspect
 import textwrap
 from silica.python_backend import PyFSM
-from silica.cfg import ControlFlowGraph
+from silica.cfg import ControlFlowGraph, Yield, BasicBlock, Branch
 from silica.ast_utils import *
 from silica.transformations import desugar_for_loops
 
-def contains_yield(statements):
-    return any(map(
-        lambda x: isinstance(x, ast.Call) and
-                  isinstance(x.func, ast.Name) and
-                  x.func.id == "yield"))
+def find_paths(block):
+    if isinstance(block, Yield):
+        return [[]]
+    paths = []
+    for sink, _ in block.outgoing_edges:
+        for path in find_paths(sink):
+            paths.append([sink] + path)
+    return paths
+
 
 def find_paths_between_yields(cfg):
+    paths = []
     for block in cfg.blocks:
-        if contains_yield(block.statements):
-            seen = set()
-
+        if isinstance(block, Yield):
+            for sink, _ in block.outgoing_edges:
+                paths.extend(find_paths(sink))
+    return paths
 
 
 class FSM:
@@ -26,8 +32,16 @@ class FSM:
         tree = get_ast(f)
         tree = desugar_for_loops(tree)
         cfg = ControlFlowGraph(tree)
-        cfg.render()
-        # paths = find_paths_between_yields(cfg)
+        paths = find_paths_between_yields(cfg)
+        for i, path in enumerate(paths):
+            print("Path {}".format(i))
+            for block in path:
+                if isinstance(block, BasicBlock):
+                    for stmt in block.statements:
+                        print(astor.to_source(stmt).rstrip())
+                elif isinstance(block, Branch):
+                    print("BRANCH: " + astor.to_source(block.cond).rstrip())
+            print()
 
 def fsm(mode_or_fn):
     if isinstance(mode_or_fn, str):
