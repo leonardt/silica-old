9621a9bf8bd86bb4784d6acdf8dc0585432fde55
Lenny Truong <leonard@truong.io>
2017-03-01 10:18:28 -0800

    Improve type system test coverage

--- test/test_type_system.py
+++ test/test_type_system.py
@@ -36,3 +36,70 @@ def test_mismatched_width():
         assert False, "Program should throw TypeError"
     except TypeError as e:
         assert str(e) == "Mismatched width, trying to assign expression `{}` of width {} to variable `{}` of width {}".format("(a + 1)", 4, "b", 3)
+
+
+def test_mod():
+    try:
+        @fsm
+        def bad_width(a : Input[4], b : Output[1]):
+            while True:
+                yield
+                b = a % 1
+    except TypeError as e:
+        assert False, "Program should not throw TypeError"
+
+
+def test_bool_op_width():
+    try:
+        @fsm
+        def bad_width(a : Input[1], b : Input[2], c : Input[3], d : Output[2]):
+            while True:
+                yield
+                d = a or b or c
+        assert False, "Program should throw TypeError"
+    except TypeError as e:
+        assert str(e) == "Mismatched width, trying to assign expression `(a or b or c)` of width 3 to variable `d` of width 2"
+
+
+def test_compare_width():
+    try:
+        @fsm
+        def bad_width(a : Input[1], b : Input[2], c : Output[1]):
+            while True:
+                yield
+                c = a < b
+    except TypeError as e:
+        assert False, "Program should not throw TypeError"
+
+
+def test_subscript():
+    try:
+        @fsm
+        def bad_width(a : Input[2], b : Input[2], c : Output[2]):
+            while True:
+                yield
+                c[0] = a + b
+    except TypeError as e:
+        assert str(e) == "Mismatched width, trying to assign expression `(a + b)` of width 2 to variable `c[0]` of width 1"
+
+
+def test_bad_read_from_subscript():
+    try:
+        @fsm
+        def bad_width(a : Input[2], b : Input[2], c : Output[2]):
+            while True:
+                yield
+                c[1] = c[0]
+    except TypeError as e:
+        assert str(e) == "Cannot read from `c` with type Output"
+
+
+def test_bad_write_to_subscript():
+    try:
+        @fsm
+        def bad_width(a : Input[2], b : Input[2], c : Output[2]):
+            while True:
+                yield
+                b[1] = a[0]
+    except TypeError as e:
+        assert str(e) == "Cannot write to `b` with type Input"
