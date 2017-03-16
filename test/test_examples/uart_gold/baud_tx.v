2ee7299a04a5d57b2be7a6eac83266af336a5d1b
Lenny Truong <leonard@truong.io>
2017-03-16 12:38:49 -0700

    Update regression test

--- test/test_examples/uart_gold/baud_tx.v
+++ test/test_examples/uart_gold/baud_tx.v
@@ -5,22 +5,22 @@ initial begin
 end
 reg [9:0] i;
 always @(posedge CLKIN) begin
-if ((yield_state == 0)) begin 
-    yield_state <= 1;
-end
-if ((yield_state == 1)) begin 
-    yield_state <= 2;
-    out <= 0;
-    i <= 0;
-end
-if ((yield_state == 2) && (i + 1 < 414)) begin 
-    yield_state <= 2;
-    i <= i + 1;
-end
-if ((yield_state == 2) && (!(i + 1 < 414))) begin 
-    yield_state <= 1;
-    i <= i + 1;
-    out <= 1;
-end
+    if ((yield_state == 0)) begin 
+        yield_state <= 1;
+    end
+    if ((yield_state == 1)) begin 
+        yield_state <= 2;
+        out <= 0;
+        i <= 0;
+    end
+    if ((yield_state == 2) && (i + 1 < 414)) begin 
+        yield_state <= 2;
+        i <= i + 1;
+    end
+    if ((yield_state == 2) && (!(i + 1 < 414))) begin 
+        yield_state <= 1;
+        i <= i + 1;
+        out <= 1;
+    end
 end
 endmodule
