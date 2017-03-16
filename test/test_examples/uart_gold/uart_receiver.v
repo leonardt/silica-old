54e6b769496a8b2fc86be9b370966405837f67be
Lenny Truong <leonard@truong.io>
2017-03-16 12:38:49 -0700

    Update regression test

--- test/test_examples/uart_gold/uart_receiver.v
+++ test/test_examples/uart_gold/uart_receiver.v
@@ -9,74 +9,74 @@ reg [4:0] ____x2;
 reg [4:0] ____x3;
 reg [4:0] i;
 always @(posedge CLKIN) if (clock_enable) begin
-if ((yield_state == 0)) begin 
-    yield_state <= 1;
-end
-if ((yield_state == 1) && (ready & !rx)) begin 
-    yield_state <= 2;
-    valid <= 0;
-    ____x0 <= 0;
-end
-if ((yield_state == 1) && (!(ready & !rx))) begin 
-    yield_state <= 1;
-    valid <= 0;
-end
-if ((yield_state == 2) && (____x0 + 1 < 8)) begin 
-    yield_state <= 2;
-    ____x0 <= ____x0 + 1;
-end
-if ((yield_state == 2) && (!(____x0 + 1 < 8))
- && (!rx)) begin 
-    yield_state <= 3;
-    ____x0 <= ____x0 + 1;
-    i <= 0;
-    ____x1 <= 0;
-end
-if ((yield_state == 2) && (!(____x0 + 1 < 8))
- && (!!rx)) begin 
-    yield_state <= 1;
-    ____x0 <= ____x0 + 1;
-end
-if ((yield_state == 3) && (____x1 + 1 < 15)) begin 
-    yield_state <= 3;
-    ____x1 <= ____x1 + 1;
-end
-if ((yield_state == 3) && (!(____x1 + 1 < 15))) begin 
-    yield_state <= 4;
-    ____x1 <= ____x1 + 1;
-    data[i] <= rx;
-end
-if ((yield_state == 4) && (i + 1 < 8)) begin 
-    yield_state <= 3;
-    i <= i + 1;
-    ____x1 <= 0;
-end
-if ((yield_state == 4) && (!(i + 1 < 8))) begin 
-    yield_state <= 5;
-    i <= i + 1;
-    ____x2 <= 0;
-end
-if ((yield_state == 5) && (____x2 + 1 < 15)) begin 
-    yield_state <= 5;
-    ____x2 <= ____x2 + 1;
-end
-if ((yield_state == 5) && (!(____x2 + 1 < 15))) begin 
-    yield_state <= 6;
-    ____x2 <= ____x2 + 1;
-    valid <= rx;
-end
-if ((yield_state == 6)) begin 
-    yield_state <= 7;
-    valid <= 0;
-    ____x3 <= 0;
-end
-if ((yield_state == 7) && (____x3 + 1 < 14)) begin 
-    yield_state <= 7;
-    ____x3 <= ____x3 + 1;
-end
-if ((yield_state == 7) && (!(____x3 + 1 < 14))) begin 
-    yield_state <= 1;
-    ____x3 <= ____x3 + 1;
-end
+    if ((yield_state == 0)) begin 
+        yield_state <= 1;
+    end
+    if ((yield_state == 1) && (ready & !rx)) begin 
+        yield_state <= 2;
+        valid <= 0;
+        ____x0 <= 0;
+    end
+    if ((yield_state == 1) && (!(ready & !rx))) begin 
+        yield_state <= 1;
+        valid <= 0;
+    end
+    if ((yield_state == 2) && (____x0 + 1 < 8)) begin 
+        yield_state <= 2;
+        ____x0 <= ____x0 + 1;
+    end
+    if ((yield_state == 2) && (!(____x0 + 1 < 8))
+     && (!rx)) begin 
+        yield_state <= 3;
+        ____x0 <= ____x0 + 1;
+        i <= 0;
+        ____x1 <= 0;
+    end
+    if ((yield_state == 2) && (!(____x0 + 1 < 8))
+     && (!!rx)) begin 
+        yield_state <= 1;
+        ____x0 <= ____x0 + 1;
+    end
+    if ((yield_state == 3) && (____x1 + 1 < 15)) begin 
+        yield_state <= 3;
+        ____x1 <= ____x1 + 1;
+    end
+    if ((yield_state == 3) && (!(____x1 + 1 < 15))) begin 
+        yield_state <= 4;
+        ____x1 <= ____x1 + 1;
+        data[i] <= rx;
+    end
+    if ((yield_state == 4) && (i + 1 < 8)) begin 
+        yield_state <= 3;
+        i <= i + 1;
+        ____x1 <= 0;
+    end
+    if ((yield_state == 4) && (!(i + 1 < 8))) begin 
+        yield_state <= 5;
+        i <= i + 1;
+        ____x2 <= 0;
+    end
+    if ((yield_state == 5) && (____x2 + 1 < 15)) begin 
+        yield_state <= 5;
+        ____x2 <= ____x2 + 1;
+    end
+    if ((yield_state == 5) && (!(____x2 + 1 < 15))) begin 
+        yield_state <= 6;
+        ____x2 <= ____x2 + 1;
+        valid <= rx;
+    end
+    if ((yield_state == 6)) begin 
+        yield_state <= 7;
+        valid <= 0;
+        ____x3 <= 0;
+    end
+    if ((yield_state == 7) && (____x3 + 1 < 14)) begin 
+        yield_state <= 7;
+        ____x3 <= ____x3 + 1;
+    end
+    if ((yield_state == 7) && (!(____x3 + 1 < 14))) begin 
+        yield_state <= 1;
+        ____x3 <= ____x3 + 1;
+    end
 end
 endmodule
--- test/test_examples/uart_gold/uart_transmitter.v
+++ test/test_examples/uart_gold/uart_transmitter.v
@@ -5,49 +5,49 @@ initial begin
 end
 reg [4:0] i;
 always @(posedge CLKIN) if (clock_enable) begin
-if ((yield_state == 0) && valid) begin 
-    yield_state <= 1;
-    ready <= 1;
-end
-if ((yield_state == 0) && (!valid)) begin 
-    yield_state <= 5;
-    tx <= 1;
-end
-if ((yield_state == 1)) begin 
-    yield_state <= 2;
-    ready <= 0;
-    tx <= 0;
-end
-if ((yield_state == 2)) begin 
-    yield_state <= 3;
-    i <= 0;
-    tx <= data[0];
-end
-if ((yield_state == 3) && (i + 1 < 8)) begin 
-    yield_state <= 3;
-    i <= i + 1;
-    tx <= data[i + 1];
-end
-if ((yield_state == 3) && (!(i + 1 < 8))) begin 
-    yield_state <= 4;
-    i <= i + 1;
-    tx <= 1;
-end
-if ((yield_state == 4) && valid) begin 
-    yield_state <= 1;
-    ready <= 1;
-end
-if ((yield_state == 4) && (!valid)) begin 
-    yield_state <= 5;
-    tx <= 1;
-end
-if ((yield_state == 5) && valid) begin 
-    yield_state <= 1;
-    ready <= 1;
-end
-if ((yield_state == 5) && (!valid)) begin 
-    yield_state <= 5;
-    tx <= 1;
-end
+    if ((yield_state == 0) && valid) begin 
+        yield_state <= 1;
+        ready <= 1;
+    end
+    if ((yield_state == 0) && (!valid)) begin 
+        yield_state <= 5;
+        tx <= 1;
+    end
+    if ((yield_state == 1)) begin 
+        yield_state <= 2;
+        ready <= 0;
+        tx <= 0;
+    end
+    if ((yield_state == 2)) begin 
+        yield_state <= 3;
+        i <= 0;
+        tx <= data[0];
+    end
+    if ((yield_state == 3) && (i + 1 < 8)) begin 
+        yield_state <= 3;
+        i <= i + 1;
+        tx <= data[i + 1];
+    end
+    if ((yield_state == 3) && (!(i + 1 < 8))) begin 
+        yield_state <= 4;
+        i <= i + 1;
+        tx <= 1;
+    end
+    if ((yield_state == 4) && valid) begin 
+        yield_state <= 1;
+        ready <= 1;
+    end
+    if ((yield_state == 4) && (!valid)) begin 
+        yield_state <= 5;
+        tx <= 1;
+    end
+    if ((yield_state == 5) && valid) begin 
+        yield_state <= 1;
+        ready <= 1;
+    end
+    if ((yield_state == 5) && (!valid)) begin 
+        yield_state <= 5;
+        tx <= 1;
+    end
 end
 endmodule
