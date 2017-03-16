cb38b7e572c6d79ef8a9159e64fc1d2bf29727b3
Lenny Truong <leonardtruong@protonmail.com>
2017-03-16 13:10:01 -0700

    Support verilator test for uart

--- test/test_examples/uart_gold/baud_rx.v
+++ test/test_examples/uart_gold/baud_rx.v
@@ -3,24 +3,24 @@ reg [1:0] yield_state;
 initial begin
     yield_state = 0;
 end
-reg [5:0] i;
+reg [4:0] i;
 always @(posedge CLKIN) begin
-if ((yield_state == 0)) begin 
-    yield_state <= 1;
-end
-if ((yield_state == 1)) begin 
-    yield_state <= 2;
-    out <= 0;
-    i <= 0;
-end
-if ((yield_state == 2) && (i + 1 < 24)) begin 
-    yield_state <= 2;
-    i <= i + 1;
-end
-if ((yield_state == 2) && (!(i + 1 < 24))) begin 
-    yield_state <= 1;
-    i <= i + 1;
-    out <= 1;
-end
+    if ((yield_state == 0)) begin 
+        yield_state <= 1;
+    end
+    if ((yield_state == 1)) begin 
+        yield_state <= 2;
+        out <= 0;
+        i <= 0;
+    end
+    if ((yield_state == 2) && (i + 1 < 24)) begin 
+        yield_state <= 2;
+        i <= i + 1;
+    end
+    if ((yield_state == 2) && (!(i + 1 < 24))) begin 
+        yield_state <= 1;
+        i <= i + 1;
+        out <= 1;
+    end
 end
 endmodule
--- test/test_examples/uart_gold/uart_transmitter.v
+++ test/test_examples/uart_gold/uart_transmitter.v
@@ -3,51 +3,51 @@ reg [3:0] yield_state;
 initial begin
     yield_state = 0;
 end
-reg [4:0] i;
+reg [2:0] i;
 always @(posedge CLKIN) if (clock_enable) begin
-if ((yield_state == 0) && valid) begin 
-    yield_state <= 1;
-    ready <= 1;
-end
-if ((yield_state == 0) && (!valid)) begin 
-    yield_state <= 5;
-    tx <= 1;
-end
-if ((yield_state == 1)) begin 
-    yield_state <= 2;
-    ready <= 0;
-    tx <= 0;
-end
-if ((yield_state == 2)) begin 
-    yield_state <= 3;
-    i <= 0;
-    tx <= data[0];
-end
-if ((yield_state == 3) && (i + 1 < 8)) begin 
-    yield_state <= 3;
-    i <= i + 1;
-    tx <= data[i + 1];
-end
-if ((yield_state == 3) && (!(i + 1 < 8))) begin 
-    yield_state <= 4;
-    i <= i + 1;
-    tx <= 1;
-end
-if ((yield_state == 4) && valid) begin 
-    yield_state <= 1;
-    ready <= 1;
-end
-if ((yield_state == 4) && (!valid)) begin 
-    yield_state <= 5;
-    tx <= 1;
-end
-if ((yield_state == 5) && valid) begin 
-    yield_state <= 1;
-    ready <= 1;
-end
-if ((yield_state == 5) && (!valid)) begin 
-    yield_state <= 5;
-    tx <= 1;
-end
+    if ((yield_state == 0) && valid) begin 
+        yield_state <= 1;
+        ready <= 1;
+    end
+    if ((yield_state == 0) && (!valid)) begin 
+        yield_state <= 5;
+        tx <= 1;
+    end
+    if ((yield_state == 1)) begin 
+        yield_state <= 2;
+        ready <= 0;
+        tx <= 0;
+    end
+    if ((yield_state == 2)) begin 
+        yield_state <= 3;
+        i <= 0;
+        tx <= data[0];
+    end
+    if ((yield_state == 3) && (i + 1 < 8)) begin 
+        yield_state <= 3;
+        i <= i + 1;
+        tx <= data[i + 1];
+    end
+    if ((yield_state == 3) && (!(i + 1 < 8))) begin 
+        yield_state <= 4;
+        i <= i + 1;
+        tx <= 1;
+    end
+    if ((yield_state == 4) && valid) begin 
+        yield_state <= 1;
+        ready <= 1;
+    end
+    if ((yield_state == 4) && (!valid)) begin 
+        yield_state <= 5;
+        tx <= 1;
+    end
+    if ((yield_state == 5) && valid) begin 
+        yield_state <= 1;
+        ready <= 1;
+    end
+    if ((yield_state == 5) && (!valid)) begin 
+        yield_state <= 5;
+        tx <= 1;
+    end
 end
 endmodule
