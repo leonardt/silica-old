b6736066e2649b53396b6e34f3a6e1ee4457e7aa
Lenny Truong <leonardtruong@protonmail.com>
2017-03-17 11:12:35 -0700

    Test uart with simulator

--- test/test_examples/test_uart.py
+++ test/test_examples/test_uart.py
@@ -6,12 +6,4 @@ def test():
     file_dir = os.path.dirname(__file__)
     package_dir = os.path.dirname(os.path.dirname(file_dir))
     call(['python3', os.path.join(package_dir, 'examples/uart/uart.py')])
-    assert filecmp.cmp(os.path.join(package_dir, "examples/uart/uart_transmitter.v"), 
-                       os.path.join(file_dir, "uart_gold/uart_transmitter.v"), shallow=False)
-    assert filecmp.cmp(os.path.join(package_dir, "examples/uart/uart_receiver.v"), 
-                       os.path.join(file_dir, "uart_gold/uart_receiver.v"), shallow=False)
-    assert filecmp.cmp(os.path.join(package_dir, "examples/uart/baud_tx.v"), 
-                       os.path.join(file_dir, "uart_gold/baud_tx.v"), shallow=False)
-    assert filecmp.cmp(os.path.join(package_dir, "examples/uart/baud_rx.v"), 
-                       os.path.join(file_dir, "uart_gold/baud_rx.v"), shallow=False)
-
+    assert call(['make', '-C', os.path.join(package_dir, 'examples/uart'), 'sim_tx']) == 0
