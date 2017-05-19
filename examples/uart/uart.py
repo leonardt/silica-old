7df68046b98559df6359c3479fc805c7ef83eb21
Lenny Truong <leonardtruong@protonmail.com>
2017-05-19 15:33:49 -0700

    Add simulated uart to test suite

--- examples/uart/uart.py
+++ examples/uart/uart.py
@@ -26,8 +26,8 @@ def uart_transmitter(data : In(Array(8, Bit)), valid : In(Bit), tx : Out(Bit)):
 icestick = IceStick()
 icestick.Clock.on()
 icestick.TX.output().on()
-for i in range(len(icestick.PMOD0)):
-    icestick.PMOD0[i].output().on()
+# for i in range(len(icestick.PMOD0)):
+#     icestick.PMOD0[i].output().on()
 # for i in range(len(icestick.PMOD1)):
 #     icestick.PMOD1[i].output().on()
 main = icestick.main()
@@ -36,14 +36,15 @@ uart = uart_transmitter()
 wire(uart.data, int2seq(85, 8))
 wire(uart.valid, 1)
 wire(uart.tx, main.TX)
-wire(uart.tx, main.PMOD0[0])
-wire(uart.tx, main.PMOD0[1])
-wire(uart.tx, main.PMOD0[2])
-wire(uart.tx, main.PMOD0[3])
+wire(uart.CE, baud_clock.COUT)
+# wire(uart.tx, main.PMOD0[0])
+# wire(uart.tx, main.PMOD0[1])
+# wire(uart.tx, main.PMOD0[2])
+# wire(uart.tx, main.PMOD0[3])
 # wire(uart.state_out[3], main.PMOD1[0])
 # wire(uart.state_out[4], main.PMOD1[1])
 # wire(uart.state_out[5], main.PMOD1[2])
 # wire(uart.state_out[6], main.PMOD1[3])
-wire(uart.CE, baud_clock.COUT)
 
-compile("uart", main)
+if __name__ == '__main__':
+    compile("uart", main)
---
+++ test/test_examples.py
@@ -0,0 +1,25 @@
+from magma import *
+from magma.python_simulator import PythonSimulator
+from magma.scope import Scope
+
+def test_uart():
+    import sys
+    sys.path.append('../examples')
+    from examples.uart.uart import main, uart
+
+    simulator = PythonSimulator(main)
+    # TODO: Why do we need to warm up with two clock cycles?
+    for _ in range(2):
+        for j in range(103 * 2):
+            simulator.step()
+            simulator.evaluate()
+    scope = Scope()
+    expected = [0] + int2seq(85, 8) + [1]
+    for h in range(2):
+        actual = []
+        for _ in range(len(expected)):
+            for j in range(103 * 2):
+                simulator.step()
+                simulator.evaluate()
+            actual.append(int(simulator.get_value(uart.tx, scope)))
+        assert expected == actual, str(expected) + str(actual)
