9027bc8ffae0222d71633b91398ff7d87d83661b
Lenny Truong <leonard@truong.io>
2017-06-09 12:15:06 -0700

    Uart example updates

--- examples/uart/monitor.py
+++ examples/uart/monitor.py
@@ -1,10 +1,11 @@
 import serial
 import time
 import sys
+from uart import BAUD_RATE, message
 
 # argument should be something like
 # /dev/tty.usbserial-142B
 # /dev/ttyUSB1
-with serial.Serial(sys.argv[1], 115200, timeout=1) as ser:
+with serial.Serial(sys.argv[1], BAUD_RATE, timeout=1) as ser:
     while True:
-        print(ser.read(10))
+        print(ser.read(len(message) * 5).decode(), end="")
--- examples/uart/uart.py
+++ examples/uart/uart.py
@@ -7,6 +7,32 @@ BAUD_RATE  = 115200
 CLOCK_RATE = int(12e6)  # 12 mhz
 
 
+def REGs(n):
+    return [Register(8) for i in range(n)]
+
+def MUXs(n):
+    return [Mux(2,8) for i in range(n)]
+
+def _ROM(logn, init, A):
+    n = 1 << logn
+    assert len(A) == logn
+    assert len(init) == n
+
+    muxs = MUXs(n-1)
+    for i in range(n//2):
+        muxs[i](init[2*i], init[2*i+1], A[0])
+
+    k = 0
+    l = 1 << (logn-1)
+    for i in range(logn-1):
+        for j in range(l//2):
+            muxs[k+l+j](muxs[k+2*j], muxs[k+2*j+1], A[i+1])
+        k += l
+        l //= 2
+
+    return muxs[n-2]
+
+
 @fsm(clock_enable=True)
 def uart_transmitter(data : In(Array(8, Bit)), valid : In(Bit), tx : Out(Bit)):
     while True:
@@ -17,6 +43,9 @@ def uart_transmitter(data : In(Array(8, Bit)), valid : In(Bit), tx : Out(Bit)):
                 tx = data[i]
                 yield
             tx = 1  # end bit
+            yield
+            # Holding the line high for an extra baud cycle seems to improve
+            # reliability of transmission (reduces framing issues?)
             yield
         else:
             tx = 1
@@ -29,7 +58,18 @@ icestick.TX.output().on()
 main = icestick.main()
 baud_clock = CounterModM(103, 8)
 uart = uart_transmitter()
-wire(uart.data, int2seq(94, 8))
+advance = CounterModM(11, 4, ce=True)
+wire(advance.CE, baud_clock.COUT)
+counter = Counter(4, ce=True)
+wire(counter.CE, advance.COUT)
+message = "Hello, world! \r\n"
+rom = _ROM(4, [array(*int2seq(ord(char), 8)) for char in message], counter.O)
+# rom = _ROM(4, list(reversed([array(*int2seq(ord(char), 8)) for char in "Hello, world! \r\n"])), counter.O)
+wire(uart.data, rom.O)
+# wire(uart.data[0:2], rom.O)
+# wire(uart.data[2:8], [0 for _ in range(6)])
+# wire(uart.data, list(reversed(int2seq(72, 8))))
+# wire(uart.data, int2seq(72, 8))
 wire(uart.valid, 1)
 wire(uart.tx, main.TX)
 wire(uart.CE, baud_clock.COUT)
