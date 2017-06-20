db1f7c63b3d4b692f7ae5e1aab69c8a0fe9e0590
Lenny Truong <leonard@truong.io>
2017-06-20 09:40:14 -0700

    WIP printf example

---
+++ examples/printf/magma/monitor.py
@@ -0,0 +1,11 @@
+import serial
+import time
+import sys
+from printf import BAUD_RATE, message
+
+# argument should be something like
+# /dev/tty.usbserial-142B
+# /dev/ttyUSB1
+with serial.Serial(sys.argv[1], BAUD_RATE, timeout=1) as ser:
+    while True:
+        print(ser.read(len(message) * 5).decode(), end="")
