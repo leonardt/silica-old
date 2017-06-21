7c32959376875a6a7d452cf41ee80b2c33dfd5bb
Lenny Truong <leonardtruong@protonmail.com>
2017-06-21 16:00:41 -0700

    Use a control signal instead of counters for message sender

--- examples/printf/magma/printf.py
+++ examples/printf/magma/printf.py
@@ -9,7 +9,10 @@ CLOCK_RATE = int(12e6)  # 12 mhz
 
 
 @fsm(clock_enable=True)
-def uart_transmitter(data : In(Array(8, Bit)), run : In(Bit), tx : Out(Bit)):
+def uart_transmitter(data : In(Array(8, Bit)), 
+                     run  : In(Bit), 
+                     tx   : Out(Bit),
+                     done : Out(Bit)):
     while True:
         if run:
             tx = 0  # start bit
@@ -19,9 +22,11 @@ def uart_transmitter(data : In(Array(8, Bit)), run : In(Bit), tx : Out(Bit)):
                 yield
             tx = 1  # end bit
             yield
+            done = 1
             # Holding the line high for an extra baud cycle seems to improve
             # reliability of transmission (reduces framing issues?)
             yield
+            done = 0
         else:
             tx = 1
             yield
@@ -43,35 +48,36 @@ def send_message(send    : In(Bit),
                  message : In(Array(8, Bit)),
                  id      : In(Array(8, Bit)),
                  out     : Out(Array(8, Bit)),
-                 run     : Out(Bit)):
+                 run     : Out(Bit),
+                 done    : In(Bit)):
     while True:
         if send:
             out = START_BYTE
             run = 1
             yield
             run = 0
-            for i in range(10):
+            while not done:
                 yield
 
             out = id
             run = 1
             yield
             run = 0
-            for i in range(10):
+            while not done:
                 yield
 
             out = message
             run = 1
             yield
             run = 0
-            for i in range(10):
+            while not done:
                 yield
 
             out = END_BYTE
             run = 1
             yield
             run = 0
-            for i in range(10):
+            while not done:
                 yield
         else:
             yield
@@ -100,6 +106,7 @@ def printf(message, arg, when=None):
     wire(data.O, sender.message)
     wire(int2seq(id, 8), sender.id)
     wire(uart.data, sender.out)
+    wire(uart.done, sender.done)
     wire(uart.run, sender.run)
     wire(uart.tx, main.TX)
     wire(uart.CE, baud_clock.COUT)
